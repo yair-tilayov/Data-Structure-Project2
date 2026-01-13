6ddbb1f633699ad28d91130975d143f0f516ba62
yair-tilayov <yairtilayov@gmail.com>
2026-01-13 12:01:33 +0200

    meld seems ok

--- SelfTest.java
+++ SelfTest.java
@@ -2,7 +2,7 @@
 public class SelfTest {
     
     public static void main(String[] args) {
-        Heap heap = new Heap(true, false);
+        Heap heap = new Heap(false, false);
         Heap.HeapItem[] items = new Heap.HeapItem[1000];
         for (int i = 0; i < 1000; i++) {
             items[i] = heap.insert(i, "a");
@@ -10,6 +10,15 @@ public class SelfTest {
         heap.deleteMin();
         heap.decreaseKey(items[100], 99);
         heap.delete(items[100]);
+
+        Heap heap2 = new Heap(false, false);
+        Heap.HeapItem[] items2 = new Heap.HeapItem[1000];
+        for (int i = 1000; i < 2000; i++) {
+            items[i-1000] = heap.insert(i, "a");
+        }
+
+        heap.meld(heap2);
+
         //System.out.println();
         System.out.println("size: " + heap.size());
         System.out.println("trees number: " + heap.numTrees());
